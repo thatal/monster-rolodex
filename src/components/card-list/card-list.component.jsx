2172370d8172ee8d60e61ae400eb9546bff4d679
Sunil Thatal <sunilthatal007@gmail.com>
2019-11-08 07:53:25 +0530

    data paased to another component

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -3,6 +3,12 @@ import './card-list.style.css';
 export const CardList = props => {
     console.log(props);
     return (
-        <div className="card-list">{props.children}</div>
+        <div className="card-list">
+            {
+                props.monsters.map(monster => (
+                    <h1 key={monster.id}>{monster.name}</h1>
+                ))
+            }
+        </div>
     );
 }
