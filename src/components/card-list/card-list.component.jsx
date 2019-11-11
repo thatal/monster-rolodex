d668d6a4a868066bc3078cefb92c7f8cfcf44056
Sunil Thatal <sunilthatal007@gmail.com>
2019-11-11 07:41:02 +0530

    Log Removed

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -2,8 +2,7 @@ import React from 'react'
 import {Card} from '../card/card.component'
 import './card-list.style.css';
 export const CardList = props => {
-    console.log(props);
-    return (
+   return (
         <div className="card-list">
             {
                 props.monsters.map(monster => (
