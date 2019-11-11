1554ca8e5104d2a432fd496870b1d4e0ac3788b2
Sunil Thatal <sunilthatal007@gmail.com>
2019-11-11 10:50:37 +0530

    Updates

---
+++ precache-manifest.a92a7fd33f1dfafed143dd0ddc581544.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f3348a91ede766406bd2f520ea10c8d",
+    "url": "https//thatal.github.io/monster-rolodex/index.html"
+  },
+  {
+    "revision": "faddc8649294acc4608d",
+    "url": "https//thatal.github.io/monster-rolodex/static/css/main.414adb38.chunk.css"
+  },
+  {
+    "revision": "391cb0e4e182aa14b4b0",
+    "url": "https//thatal.github.io/monster-rolodex/static/js/2.5430253d.chunk.js"
+  },
+  {
+    "revision": "faddc8649294acc4608d",
+    "url": "https//thatal.github.io/monster-rolodex/static/js/main.e9fb0871.chunk.js"
+  },
+  {
+    "revision": "ba61b9ef1d1c64459876",
+    "url": "https//thatal.github.io/monster-rolodex/static/js/runtime-main.dc6e1078.js"
+  }
+]);
