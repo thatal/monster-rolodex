f79d38ec4abaeb0046bbc00182e959cb8e5911c5
Sunil Thatal <sunilthatal007@gmail.com>
2019-11-11 11:02:15 +0530

    Updates

---
+++ precache-manifest.0039c86869ace3bce1e107907cf871b9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "659968e55824f940bad2a0be9d26a520",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "39a84d60e9d6d10a088b",
+    "url": "/monster-rolodex/static/css/main.414adb38.chunk.css"
+  },
+  {
+    "revision": "391cb0e4e182aa14b4b0",
+    "url": "/monster-rolodex/static/js/2.5430253d.chunk.js"
+  },
+  {
+    "revision": "39a84d60e9d6d10a088b",
+    "url": "/monster-rolodex/static/js/main.dd008217.chunk.js"
+  },
+  {
+    "revision": "aff08a4d83f58743dcb3",
+    "url": "/monster-rolodex/static/js/runtime-main.400ce9bc.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https//thatal.github.io/monster-rolodex/precache-manifest.7d96f1cbee1f49ce0a6768d8b93d6f81.js"
+  "/monster-rolodex/precache-manifest.0039c86869ace3bce1e107907cf871b9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https//thatal.github.io/monster-rolodex/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/monster-rolodex/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
